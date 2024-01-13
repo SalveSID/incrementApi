ee67627e65a7813d112a2519129930138e155979
SalveSID <125227695+SalveSID@users.noreply.github.com>
2024-01-13 13:57:27 -0400

    commit 1

---
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @PostMapping("/increment")
+    public Payload incrementData(@RequestBody Payload payload) {
+
+        // Update the string value to be the concatenation of itself
+        payload.setStringValue(payload.getStringValue() + " " + payload.getStringValue());
+
+        // Increment the int value by itself
+        payload.setIntValue(payload.getIntValue() + payload.getIntValue());
+
+        // Increment the date by one day
+        payload.setDateTime(payload.getDateTime().plusDays(1));
+
+        // Return the modified payload
+        return payload;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/Payload.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+
+public class Payload {
+    private String stringValue;
+    private int intValue;
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    private LocalDateTime dateTime;
+
+
+}
